2ac5a211f31d4e6e30daa8286a93aa189fea8736
yuuichihirakawa <116409798+yuuichihirakawa@users.noreply.github.com>
2023-01-23 15:00:20 +0900

    ２回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,5 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :new, :create, :show]
   
 
   def index
