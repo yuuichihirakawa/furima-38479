e1a6a0ed469c73cbb42fc6a55967b94989c2ac4a
yuuichihirakawa <116409798+yuuichihirakawa@users.noreply.github.com>
2023-01-25 18:53:33 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_item,only: [:edit, :update, :destroy]
   
 
   def index
@@ -25,6 +26,17 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -33,4 +45,10 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :product_description, :item_category_id, :item_state_id, :item_price, :delivery_charge_burden_id, :days_up_to_delivery_id, :address_prefecture_id, :image).merge(user_id: current_user.id)
   end
 
+  def correct_item
+    @item = Item.find(params[:id])
+    unless @item.user.id == current_user.id
+      redirect_to items_path
+    end
+  end
 end
