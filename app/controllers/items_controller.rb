49dc049bc890d23881ad5572ae3e8de177edaaa0
yuuichihirakawa <116409798+yuuichihirakawa@users.noreply.github.com>
2023-02-12 16:41:57 +0900

    修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :correct_item,only: [:edit, :update, :destroy]
   
   
